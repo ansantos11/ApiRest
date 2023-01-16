ac7b4c6581e09427976d11134ba018d50b76dc91
Ansantos0912 <ansantos11@gmail.com>
2023-01-16 16:11:31 -0500

    upload first

---
+++ NetCoreYouTube/Controllers/ProductoController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreYouTube.Models;
+using NetCoreYouTube.Recursos;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace NetCoreYouTube.Controllers
+{
+    [ApiController]
+    [Route("producto")]
+    public class Productocontroller : ControllerBase
+    {
+        [HttpGet]
+        [Route("Listar")]
+        public dynamic ListarProductos() 
+        {
+            List<Parametro> parametros = new List<Parametro>
+            {
+                new Parametro("@Estado","1")
+            };
+            DataTable tCategoria = DBDatos.Listar("Categoria_Listar", parametros);
+            DataTable tProducto = DBDatos.Listar("Producto_Listar");
+
+            string jsoncategoria = JsonConvert.SerializeObject(tCategoria);
+            string jsonProducto = JsonConvert.SerializeObject(tProducto);
+
+            return new
+            {
+                success = true,
+                mesagge = "Exito",
+                Result = new
+                {
+                    categoria = JsonConvert.DeserializeObject<List<Categoria>>(jsoncategoria),
+                    producto = JsonConvert.DeserializeObject<List<Producto>>(jsonProducto),
+                    
+                }
+            }; 
+        }
+        [HttpPost]
+        [Route("agregar")]
+        public dynamic AgregarProducto(Producto producto)
+        {
+            List<Parametro> parametros = new List<Parametro>
+            {
+                new Parametro("@IDCategoria",producto.IDCategoria),
+                new Parametro("@Nombre",producto.Nombre),
+                new Parametro("@Precio",producto.Precio)
+            };
+            bool exito = DBDatos.Ejecutar("Producto_Agregar", parametros);
+            
+            return new
+            {
+                success = exito,
+                mesagge = exito ? "exito" : "Error al guardar",
+                Result = ""
+            };
+        }
+    }
+}
---
+++ NetCoreYouTube/Recursos/Parametro.cs
@@ -0,0 +1,15 @@
+﻿namespace NetCoreYouTube.Recursos
+{
+    public class Parametro
+    {
+        public Parametro( string nombre, string valor)
+        {
+            Nombre = nombre;
+            Valor = valor;
+        }
+
+        public string Nombre { get; set; }
+        public string Valor { get; set; }
+
+    }
+}
